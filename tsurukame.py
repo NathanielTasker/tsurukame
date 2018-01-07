598e06530555b3cdcca3583a582cebb638858d01
NathanielTasker <natsumit.fi@gmail.com>
2018-01-08 00:47:51 +0900

    fixed random_legs, added tsurukame() and printing result

--- tsurukame.py
+++ tsurukame.py
@@ -5,6 +5,16 @@ def random_legs():
     kame = r(100) * 4
     tsuru_legs = tsuru * 2
     kame_legs = kame * 4
-    return (tsuru + kame, tsuru_legs + kame_legs)
+    return [tsuru_legs + kame_legs, tsuru + kame]
 
-print('%s legs, %s creatures are there.' % random_legs())
+def tsurukame(x):
+    legs = x[0]
+    creatures = x[1]
+    all_tsuru = creatures * 2
+    kame = round((legs - all_tsuru)/2)
+    tsuru = round(creatures - kame)
+    return [tsuru, kame]
+
+r = random_legs()
+print('%s legs, %s creatures are there.' % tuple(r))
+print('%s tsuru, %s kame are there.' % tuple(tsurukame(r)))
