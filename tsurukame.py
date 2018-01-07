138c2d6f0bf3bf79da419fc2e60c368e64320c7a
NathanielTasker <natsumit.fi@gmail.com>
2018-01-08 00:15:44 +0900

    added random_legs() to tsurukame.py

--- tsurukame.py
+++ tsurukame.py
@@ -1,1 +1,8 @@
+from random import randrange as r
 
+def random_legs():
+    tsuru_legs = r(100) * 2
+    kame_legs = r(100) * 4
+    return tsuru_legs + kame_legs
+
+print('%s legs are there.' % random_legs())
