7a715e2a65b65254787b5f5a1b5d3821fdca2e36
NathanielTasker <natsumit.fi@gmail.com>
2018-01-08 00:53:52 +0900

    fixed random_legs()

--- tsurukame.py
+++ tsurukame.py
@@ -1,8 +1,8 @@
 from random import randrange as r
 
 def random_legs():
-    tsuru = r(100) * 2
-    kame = r(100) * 4
+    tsuru = r(100)
+    kame = r(100)
     tsuru_legs = tsuru * 2
     kame_legs = kame * 4
     return [tsuru_legs + kame_legs, tsuru + kame]
