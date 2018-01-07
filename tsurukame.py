80fe1558b19334f234a595454fb1c917c0b5bc24
NathanielTasker <natsumit.fi@gmail.com>
2018-01-08 00:30:15 +0900

    fixed random_legs()

--- tsurukame.py
+++ tsurukame.py
@@ -1,8 +1,10 @@
 from random import randrange as r
 
 def random_legs():
-    tsuru_legs = r(100) * 2
-    kame_legs = r(100) * 4
-    return tsuru_legs + kame_legs
+    tsuru = r(100) * 2
+    kame = r(100) * 4
+    tsuru_legs = tsuru * 2
+    kame_legs = kame * 4
+    return (tsuru + kame, tsuru_legs + kame_legs)
 
-print('%s legs are there.' % random_legs())
+print('%s legs, %s creatures are there.' % random_legs())
